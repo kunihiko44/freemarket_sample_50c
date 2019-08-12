a3b6fcd28965e8db29eed4c05afd03c854e016b8
kunihiko44 <kunihiko.yoshida.hk@gmail.com>
2019-08-13 00:45:44 +0900

    imageモデルの作成
    rails g modelでimageモデルを作成した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_08_200342) do
+ActiveRecord::Schema.define(version: 2019_08_12_154000) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 2019_08_08_200342) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_categories_on_name"
+  end
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
