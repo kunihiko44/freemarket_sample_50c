e6890fef2086d29c76b808cffce8893c47e5cada
kunihiko44 <kunihiko.yoshida.hk@gmail.com>
2019-08-19 22:16:27 +0900

    routingをresourcesで記述。
    routingをresourcesで記述し、
    /sell/のパスで、移動できるようにした。

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'products#index'
 
   #商品ページ
-  get 'sell', to: 'products#new'
-  post 'sell', to: 'products#create'
-  resources :products
+  get 'sell', to: 'products#new', as: :new_product
+  post 'sell', to: 'products#create', as: :create_product
+  resources :products, only: [:new,:create] do
+  end
 end
