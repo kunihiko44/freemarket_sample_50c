95e4053799ff7657378724d4966fd4dc664c520f
kunihiko44 <kunihiko.yoshida.hk@gmail.com>
2019-08-21 16:59:53 +0900

    未記入の欄がある場合に、エラーメッセージが出るように設定
    未記入の欄がある場合に、エラーメッセージが出るように設定した。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,8 +8,33 @@ class ProductsController < ApplicationController
   end
   
   def create
-    @product = Product.new
-    @product.save
-    redirect_to new_product_path
+    @product = Product.create(product_params)
+    if @product.save
+      flash[:notice] = "出品が完了しました"
+      redirect_to controller: :products, action: :index
+    else
+      flash[:notice] = "未入力の欄があります"
+      render "new"
+    end
   end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :introduction,
+      :price,
+      :size,
+      :state_id,
+      :brand,
+      :category_id,
+      :delivery_date_id,
+      :delivery_cost_id,
+      :delivery_method_id,
+      :prefecture_id
+    )
+  end
+
+
 end
