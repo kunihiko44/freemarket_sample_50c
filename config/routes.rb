03bfa68e405a1221d20571fef897419cefdbfe06
kunihiko44 <kunihiko.yoshida.hk@gmail.com>
2019-09-01 04:05:38 +0900

    productコントローラーのnewアクションをテストした。
    単体テストで、productコントローラーのnewアクションをテストした。
    
      get 'sell', to: 'products#new', as: :new_product
      post 'sell', to: 'products#create', as: :create_product
    は、必要ないと考え削除した。

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   root 'products#index'
 
   #商品ページ
-  get 'sell', to: 'products#new', as: :new_product
-  post 'sell', to: 'products#create', as: :create_product
   resources :products, only: [:new,:create] do
   end
 
