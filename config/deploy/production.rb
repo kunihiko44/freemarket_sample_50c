fb4fb4a3fb245a57f73df1d0fd0cd8d96c98fa49
kunihiko44 <kunihiko.yoshida.hk@gmail.com>
2019-09-03 12:36:26 +0900

    デプロイを別のチームメンバーが行う時の設定に変更
    ＜deploy.rb＞
    ・リポジトリを変更
    ・公開キーを変更
    
    ＜production.rb＞
    IPアドレスを変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '13.113.228.217', user: 'ec2-user', roles: %w{app db web}
+server '52.198.157.90', user: 'ec2-user', roles: %w{app db web}
 
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
