00fbac64a138a4c87bd120b3b71c4df2415a5ca9
kunihiko44 <kunihiko.yoshida.hk@gmail.com>
2019-09-10 15:08:06 +0900

    route.rbの変更
    出品登録時、未記入の欄があると、同じページ（/sell）に戻るよう設定した。

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   root 'products#index'
 
   #商品ページ
+  get  'sell',   to: 'products#new',    as: :new_product
+  post 'sell',   to: 'products#create', as: :create_product
   resources :products, only: [:new,:create] do
   end
 
-  
+  #マイページ
   root 'mypage#index'
-  root 'items#index'
+
 
 end
