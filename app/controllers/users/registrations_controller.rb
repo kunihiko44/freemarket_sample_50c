9803c1db899c0b52c0138cbdc443cf231e29106d
OgawaTaiki <taipon93@gmail.com>
2019-10-06 19:36:55 +0900

    変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    @user = User.new(user_params)
+      if @user.save
+          render 'items/index'
+      else
+          render 'new'
+      end
+  end
 
   # GET /resource/edit
   # def edit
@@ -37,6 +42,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def cancel
   #   super
   # end
+  private
+  def user_params
+     params.require(:user).permit(:name, :email, :password, :password_confirmation, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, :postal_code, :prefecture, :city, :address, :building, :phone)
+  end
+
 
   # protected
 
