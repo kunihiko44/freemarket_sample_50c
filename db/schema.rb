5056c91c392dc022a2255d1a60000b730693c697
OgawaTaiki <taipon93@gmail.com>
2019-10-15 22:21:43 +0900

    出品ボタンでの遷移、トップページビュー修正

--- db/schema.rb
+++ db/schema.rb
@@ -46,14 +46,6 @@ ActiveRecord::Schema.define(version: 2019_10_10_134256) do
     t.index ["name"], name: "index_brands_on_name"
   end
 
-  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "customer_id", null: false
-    t.string "card_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.bigint "product_id", null: false
